82142cec7e332a60a7dcf1ce91b0196ce15f4ca1
Gustavo Sales <85498221+Gusales@users.noreply.github.com>
2024-01-18 15:08:57 -0300

    feat: created Product model
    I've created model of Product in app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLACLHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+class Product(db.Model):
+  id = db.Column(db.Integer, primay_key=True)
+  name = db.Column(db.String(120), nullable=False)
+  price = db.Column(db.Float, nullable=False)
+  description = db.Column(db.Text, nullable=True)
+
 
 @app.route('/')
 def hello_world():
