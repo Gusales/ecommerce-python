631a823ecd55021d10fc2caef22e890e61d7ec18
Gustavo Sales <85498221+Gusales@users.noreply.github.com>
2024-01-18 18:31:27 -0300

    feat: create sqlite database and register new products
    i've created the database in sqlite and create new route POST '/api/products/add' to create new products in the system.

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLACLHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
 class Product(db.Model):
-  id = db.Column(db.Integer, primay_key=True)
-  name = db.Column(db.String(120), nullable=False)
-  price = db.Column(db.Float, nullable=False)
-  description = db.Column(db.Text, nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
 
 @app.route('/')
 def hello_world():
   return 'Hello World'
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product(): 
+   data = request.json
+   if 'name' in data and 'price' in data:
+      product = Product(name = data["name"], price = data["price"], description = data.get("description", ""))
+      db.session.add(product)
+      db.session.commit()
+      return jsonify({ 'message': 'Produto cadastrado com sucesso! 👌✨✨' }), 201
+   return jsonify({ 'message': 'Invalid product data' }), 400
 
 if __name__ == "__main__":
   app.run(debug=True)
