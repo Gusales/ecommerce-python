c512c44083042fc219cf80a47994baf0324d0e62
Gustavo Sales <85498221+Gusales@users.noreply.github.com>
2024-02-01 12:50:44 -0300

    feat: maked logout route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, login_user, LoginManager, login_required
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 app = Flask(__name__)
 
@@ -47,6 +47,12 @@ def login():
       return jsonify({ 'message': 'Logged in successfully' })
    
    return jsonify({ 'message': 'Unauthorized. Invalid credentials' }), 401
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+   logout_user()
+   return jsonify({ 'message': 'Logout successfully' })
 
 @app.route('/api/products', methods=["GET"])
 def fetch_products():
